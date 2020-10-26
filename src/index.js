4814bdd1411f0ef872428a870d63073f93ee89be
Dan G <email@email.com>
2020-10-26 20:33:28 +0200

    small style change

--- src/index.js
+++ src/index.js
@@ -33,10 +33,8 @@ io.on('connection', (socket) => {
        room: user.room,
        users: getUsersInRoom(user.room)
     })
-
     
     callback()
-
   })
 
   socket.on('sendMessage', (message, callback) => {
