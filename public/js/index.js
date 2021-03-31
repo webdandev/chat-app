38a638f2c5b410fbd688e8e6c28f0bc801f5762a
Dan G <email@email.com>
2021-03-31 21:03:22 +0300

    added some encoded rooms

--- public/js/index.js
+++ public/js/index.js
@@ -4,8 +4,18 @@ const socket = io()
 joinRoomTemplate = document.querySelector('#join-room-template').innerHTML
 
 socket.on('roomsList', (rooms) => {
-  console.log(rooms)  
+  //adding encoded rooms
+  rooms.push(...addEncodedRooms(rooms, "yes, please, do reinvent the wheel!",
+    "js", "20+","general", "react.js","node.js"))  
+
   const roomNames = rooms.map((room) => ({room: room}))
   const html = Mustache.render(joinRoomTemplate, {roomNames})
   document.querySelector('#rooms').innerHTML = html    
 })
+
+const addEncodedRooms = (rooms, ...encodedrooms) => {
+  const addThoseRooms = encodedrooms.filter((room) => {
+    return rooms.indexOf(room) === -1
+  })
+  return addThoseRooms  
+}
